680c407742da35e418bb9fe66296030ff14b99dd
Red🌹🥀 <111632716+Yuyi5la@users.noreply.github.com>
2025-05-23 16:48:34 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const paymentRoutes = require('./routes/paymentRoutes');
+const paymentRoutes = require('./Routes/paymentRoutes');
 require('dotenv').config();
 
 const app = express();
